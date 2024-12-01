9d0501bda7bc1344691f74bb5fe70ec71def1f68
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-12-01 23:43:42 +0530

    Add JWT decoding and validation methods to custom app class

--- api/app.py
+++ api/app.py
@@ -5,6 +5,9 @@ from datetime import datetime, timedelta, timezone
 from sanic import Sanic
 from sanic.log import logger
 
+from api.models.internal.jwt_data import JWT_Data
+from api.models.internal.jwt_status import JWT_Status
+
 
 class API(Sanic):
     def __init__(self, *args, **kwargs):
@@ -40,6 +43,40 @@ class API(Sanic):
             kid = jwk["kid"]
             public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
         self.ctx.entra_public_keys = public_keys
+
+    def decode_jwt(self, jwt_token: str) -> JWT_Data:
+        assert isinstance(jwt_token, str)
+        data = JWT_Data(
+            **jwt.decode(jwt_token, key=self.config["PUB_KEY"], algorithms="RS256")
+        )
+        return data
+
+    def check_server_jwt(self, jwt_token: str) -> JWT_Status:
+        if not jwt_token or jwt_token == "":
+            return JWT_Status(authenticated=False, message="JWT Token not provided")
+        try:
+            jwt_data = self.decode_jwt(jwt_token)
+        except jwt.exceptions.ImmatureSignatureError:
+            # Raised when a tokens nbf claim represents a time in the future
+            d = JWT_Status(
+                authenticated=False, message="JWT Token not allowed to be used at time"
+            )
+        except jwt.exceptions.InvalidIssuedAtError:
+            # Raised when a tokens iat claim is in the future
+            d = JWT_Status(
+                authenticated=False, message="JWT Token issued in the future"
+            )
+        except jwt.exceptions.ExpiredSignatureError:
+            # Raised when a tokens exp claim indicates that it has expired
+            d = JWT_Status(authenticated=False, message="JWT Token has expired")
+        except jwt.exceptions.InvalidTokenError:
+            # Generic invalid token
+            d = JWT_Status(authenticated=False, message="JWT Token is invalid")
+        else:
+            # Valid Token
+            d = JWT_Status(authenticated=True, JWT_Data=jwt_data)
+
+        return d
 
     async def generate_jwt(
         self,
