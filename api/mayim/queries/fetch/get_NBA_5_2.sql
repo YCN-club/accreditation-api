d28676ab681ad756d1dd4b129f658ca48e308d5a
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2025-01-19 18:53:14 +0530

    Add Faculty Information fetch queries

---
+++ api/mayim/queries/fetch/get_NBA_5_2.sql
@@ -0,0 +1,17 @@
+SELECT 
+    COUNT(CASE WHEN f.highest_degree = 'DOCTORAL' THEN 1 END) AS X,
+    COUNT(CASE WHEN f.highest_degree = 'MASTERS' THEN 1 END) AS Y,
+    COUNT(*) AS F,
+    2.5 * ((10 * COUNT(CASE WHEN f.highest_degree = 'DOCTORAL' THEN 1 END) + 
+            4 * COUNT(CASE WHEN f.highest_degree = 'MASTERS' THEN 1 END)) / 
+            COUNT(*)) AS FQI
+FROM 
+    faculty f
+JOIN 
+    departments d ON f.department_id = d.id
+JOIN 
+    programs p ON d.program_id = p.id
+WHERE 
+    p.id = $program_id;
+
+-- Variables to be passed: $program_id
