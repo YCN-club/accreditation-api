5cacb80426c8b68fe58fad925650d55113e0bbaa
Abhigyan Trips <contact@abhigyantrips.dev>
2025-02-21 14:21:51 +0530

    fix(auth): structure for user ID in JWT

--- api/models/internal/login_data.py
+++ api/models/internal/login_data.py
@@ -5,19 +5,21 @@ from pydantic import BaseModel
 
 
 class LoginData(BaseModel):
-    user_id: UUID
+    uuid: UUID
     name: str
     email: str
     password: str
     is_active: bool
     requires_reset: bool
+    roles: list[str] = []
 
     def check_password(self, password: str) -> bool:
         return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
 
     def to_dict(self):
         return {
-            "user_id": str(self.user_id),
+            "uuid": str(self.uuid),
             "name": self.name,
             "email": self.email,
+            "roles": self.roles,
         }
